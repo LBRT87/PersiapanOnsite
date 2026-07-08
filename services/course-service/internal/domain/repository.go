a963bf20186f7bbb3d194ada47628e86b1318e8b
mjibranaulia <m.jibranaulia@gmail.com>
2026-07-08 16:32:29 +0700

    feat: course grpc

--- services/course-service/internal/domain/repository.go
+++ services/course-service/internal/domain/repository.go
@@ -4,15 +4,18 @@ import "context"
 
 type CourseRepository interface {
 	FindByID(c context.Context, id string) (*Course, error)
-	Create(c context.Context, course *Course)
+	Create(c context.Context, course *Course) error
 }
 type ModuleRepository interface{
-	FindByID (c context.Context, id string) (*Module, error)
+	FindByID (c context.Context, id string) (Module, error)
+	Create(c context.Context, module *Module) error
 }
 type ComponentRepository interface{
 	FindByID(c context.Context, id string) (*Component, error)
+	Create(c context.Context, component *Component) error
 }
 type EnrollmentRepository interface{
 	FindByID(c context.Context, id string) (*Enrollment, error)
-	FindByUserCourse(c context.Context, userId, lecturerId string) (*Enrollment, error)
+	FindByUserCourse(c context.Context, userId, courseId string) (bool, error)
+	Create(c context.Context, enrollment *Enrollment) error
 }
